c185e8170531331b38664017aeaf1ae1c26ec987
Tanaka de Silva <tdesilva@proda.ai>
2021-11-01 18:51:42 +0000

    FIX: Skip auth token test when running against the emulator

--- test/Cloud/PubSub/AuthSpec.hs
+++ test/Cloud/PubSub/AuthSpec.hs
@@ -2,19 +2,22 @@ module Cloud.PubSub.AuthSpec where
 
 import qualified Cloud.PubSub.Auth             as Auth
 import qualified Cloud.PubSub.Auth.Types       as AuthT
-import           Cloud.PubSub.TestHelpers       ( testServiceAccountPath )
 import qualified Network.HTTP.Client           as HttpClient
+import qualified System.Environment            as SystemEnv
 import qualified Network.HTTP.Client.TLS       as HttpClientTLS
 import           Test.Hspec
 
 tokenGetTest :: IO ()
 tokenGetTest = do
   manager        <- HttpClient.newManager HttpClientTLS.tlsManagerSettings
-  serviceAccount <- Auth.readServiceAccountFile testServiceAccountPath
-  tokenResponse  <- Auth.fetchToken manager serviceAccount scope
-  AuthT.tokenType tokenResponse `shouldBe` "Bearer"
-  AuthT.expiresIn tokenResponse `shouldSatisfy` (\t -> 3595 < t && t < 3600)
-  where scope = "https://www.googleapis.com/auth/pubsub"
+  SystemEnv.lookupEnv "GOOGLE_APPLICATION_CREDENTIALS" >>= \case
+    Nothing -> pendingWith "skipping as auth not supported in emulator"
+    Just serviceAccountPath -> do
+      serviceAccount <- Auth.readServiceAccountFile serviceAccountPath
+      tokenResponse  <- Auth.fetchToken manager serviceAccount scope
+      AuthT.tokenType tokenResponse `shouldBe` "Bearer"
+      AuthT.expiresIn tokenResponse `shouldSatisfy` (\t -> 3595 < t && t < 3600)
+      where scope = "https://www.googleapis.com/auth/pubsub"
 
 spec :: Spec
 spec = describe "Google API Auth" $ do
